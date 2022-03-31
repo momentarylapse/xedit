22b6df9663a7d677f1643ce38e11696b6448274a
Michael Ankele <michi@lupina.de>
2022-04-01 01:58:45 +0200

    drop cairo code

--- src/lib/xhui/draw/font.cpp
+++ src/lib/xhui/draw/font.cpp
@@ -4,23 +4,10 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-//#define USE_CAIRO 1
-
-
-#ifdef USE_CAIRO
-#include <cairo/cairo.h>
-//#include <pango/pango.h>
-#include <gtk/gtk.h>
-#endif
-
 namespace hui {
 
-
-void cairo_render_text(const string &font_name, float font_size, const string &text, Align align, Image &im);
-
-
-FT_Library ft2;
-FT_Face face;
+static FT_Library ft2;
+static FT_Face face;
 static int dpi = 96;
 
 
@@ -40,16 +27,6 @@ void font_init() {
 void font_set_font(const string &font_name, float font_size) {
 	FT_Set_Char_Size(face, 0, int(font_size*64.0f), dpi, dpi);
 }
-
-
-#ifdef USE_CAIRO
-float font_get_text_width(const string &font_name, float font_size, const string &text) {
-	Image im;
-	cairo_render_text(font_name, font_size * ui_scale, str, Align::LEFT, im);
-	tex_text->overwrite(im);
-	return im.width / ui_scale;
-}
-#endif
 
 float font_get_text_width(const string &font_name, float font_size, const string &text) {
 	auto utf32 = text.utf8_to_utf32();
@@ -119,77 +96,5 @@ void font_render_text(const string &font_name, float font_size, const string &te
 		x += face->glyph->advance.x >> 6;
 	}
 }
-
-#ifdef USE_CAIRO
-
-void cairo_render_text(const string &font_name, float font_size, const string &text, Align align, Image &im) {
-
-	// initial surface size guess
-	int w_surf = 200;
-	int h_surf = font_size * 2;
-
-	for (int i=0; i<2; i++) {
-
-		cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, w_surf, h_surf);
-		cairo_t *cr = cairo_create(surface);
-
-		cairo_set_source_rgba(cr, 0, 0, 0, 1);
-		cairo_rectangle(cr, 0, 0, w_surf, h_surf);
-		cairo_fill(cr);
-
-		int x = 0, y = 0;
-
-		cairo_set_source_rgba(cr, 1, 1, 1, 1);
-
-		PangoLayout *layout = pango_cairo_create_layout(cr);
-		PangoFontDescription *desc = pango_font_description_from_string((font_name + "," + f2s(font_size, 1)).c_str());
-		pango_layout_set_font_description(layout, desc);
-		pango_font_description_free(desc);
-
-		if (align & Align::RIGHT)
-			pango_layout_set_alignment(layout, PANGO_ALIGN_RIGHT);
-		else if (align & Align::CENTER_H)
-			pango_layout_set_alignment(layout, PANGO_ALIGN_CENTER);
-		else
-			pango_layout_set_alignment(layout, PANGO_ALIGN_LEFT);
-		pango_layout_set_text(layout, (char*)text.data, text.num);
-		//int baseline = pango_layout_get_baseline(layout) / PANGO_SCALE;
-		int w_used, h_used;
-		pango_layout_get_pixel_size(layout, &w_used, &h_used);
-
-
-		if ((w_used <= w_surf and h_used <= h_surf) or (i == 1)) {
-
-
-			pango_cairo_show_layout(cr, layout);
-			g_object_unref(layout);
-
-			cairo_surface_flush(surface);
-			unsigned char *c0 = cairo_image_surface_get_data(surface);
-			im.create(w_used, h_used, White);
-			for (int y=0;y<h_used;y++) {
-				unsigned char *c = c0 + 4 * y * w_surf;
-				for (int x=0;x<w_used;x++) {
-					float a = (float)c[1] / 255.0f;
-					im.set_pixel(x, y, color(a, 1, 1, 1));
-					c += 4;
-				}
-			}
-			im.alpha_used = true;
-
-			// finished
-			i = 666;
-		}
-
-		w_surf = w_used;
-		h_surf = h_used;
-		//if (w_used > w_surf)
-		//	std::cerr << "Text: too large!\n";
-		cairo_destroy(cr);
-		cairo_surface_destroy(surface);
-	}
-}
-
-#endif
 
 }
