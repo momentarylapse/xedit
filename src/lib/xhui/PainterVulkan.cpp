ffef54ee8a64bf5c62f2f55ffbc881b341d0c4d1
Michael Ankele <michi@lupina.de>
2024-10-09 18:22:09 +0200

    -b

--- src/lib/xhui/PainterVulkan.cpp
+++ src/lib/xhui/PainterVulkan.cpp
@@ -6,15 +6,115 @@
 
 #include "../vulkan/vulkan.h"
 #include "../image/image.h"
+#include "../os/msg.h"
+
+
+using namespace vulkan;
 
 
 namespace hui {
 
 
+Instance *instance = nullptr;
+DescriptorPool *pool = nullptr;
+Device *device = nullptr;
+Texture* tex_white = nullptr;
+Texture* tex_black = nullptr;
+
+
+	Fence* in_flight_fence;
+	Array<Fence*> wait_for_frame_fences;
+	Semaphore *image_available_semaphore, *render_finished_semaphore;
+
+	Array<CommandBuffer*> command_buffers;
+
+	SwapChain *swap_chain;
+	RenderPass* default_render_pass;
+	DepthBuffer* depth_buffer;
+	Array<FrameBuffer*> frame_buffers;
+	int image_index;
+	bool framebuffer_resized;
+	
+	
+	
+void _create_swap_chain_and_stuff(GLFWwindow* window) {
+	swap_chain = new vulkan::SwapChain(window, device);
+	auto swap_images = swap_chain->create_textures();
+	for (auto t: swap_images)
+		wait_for_frame_fences.add(new vulkan::Fence(device));
+
+	for (auto t: swap_images)
+		command_buffers.add(device->command_pool->create_command_buffer());
+
+	depth_buffer = swap_chain->create_depth_buffer();
+	default_render_pass = swap_chain->create_render_pass(depth_buffer);
+	frame_buffers = swap_chain->create_frame_buffers(default_render_pass, depth_buffer);
+}
+
+void api_init(GLFWwindow* window) {
+	instance = vulkan::init({"glfw", "validation", "api=1.2", "verbosity=2"});
+	device = Device::create_simple(instance, window, {"graphics", "present", "swapchain", "anisotropy", "validation"});
+	msg_write("device found");
+
+	//device->create_query_pool(MAX_TIMESTAMP_QUERIES);
+	pool = new DescriptorPool("buffer:4096,sampler:4096", 65536);
+
+	tex_white = new Texture();
+	tex_black = new Texture();
+	tex_white->write(Image(16, 16, White));
+	tex_black->write(Image(16, 16, Black));
+
+	//return new Context;
+	
+	
+
+	image_available_semaphore = new vulkan::Semaphore(device);
+	render_finished_semaphore = new vulkan::Semaphore(device);
+
+
+	framebuffer_resized = false;
+
+	_create_swap_chain_and_stuff(window);
+}
+
+
+void rebuild_default_stuff(GLFWwindow* window) {
+	msg_write("recreate swap chain");
+
+	device->wait_idle();
+
+	//_delete_swap_chain_and_stuff();
+	_create_swap_chain_and_stuff(window);
+}
+
+
 Painter::Painter(Window *w) {
+	window = w;
+	glfwMakeContextCurrent(w->window);
+	if (!instance)
+		api_init(w->window);
+		
+		
+	// start frame
+	
+	if (!swap_chain->acquire_image(&image_index, image_available_semaphore)) {
+		rebuild_default_stuff(w->window);
+		return;
+	}
+
+	auto f = wait_for_frame_fences[image_index];
+	f->wait();
+	f->reset();
 }
 
 void Painter::end() {
+
+	auto f = wait_for_frame_fences[image_index];
+	device->present_queue.submit(command_buffers[image_index], {image_available_semaphore}, {render_finished_semaphore}, f);
+
+	swap_chain->present(image_index, {render_finished_semaphore});
+
+	device->wait_idle();
 }
 
 void Painter::clear(const color &c) {
