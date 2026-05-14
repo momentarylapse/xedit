9b8fe3ab6f22bc771252ebed9a509ef1a2154be4
Michael Ankele <michi@lupina.de>
2026-05-14 02:12:56 +0200

    [lib/kaba] update

--- xedit/sharedlib/src/main.cpp
+++ xedit/sharedlib/src/main.cpp
@@ -5,15 +5,10 @@
 #include <lib/os/msg.h>
 #include <lib/xhui/xhui.h>
 #include <lib/kaba/kaba.h>
-#include <lib/kabaexport/KabaExporter.h>
+#include <lib/kapi/KabaExporter.h>
 #include <lib/kaba/Context.h>
 
 #include "lib/os/msg.h"
-
-namespace kaba {
-	class Context;
-	extern Context *_secret_lib_context_;
-}
 
 bool app_init() {
 	try {
@@ -44,11 +39,8 @@ void app_run(const Array<string>& args) {
 
 extern "C" {
 __attribute__ ((visibility ("default")))
-void export_symbols(kaba::Exporter* e) {
-	// restore/duplicate global state
-	kaba::default_context = e->ctx;
-	kaba::_secret_lib_context_ = e->secret_lib_context;
-	kaba::common_types = *e->x_common_types;
+void export_symbols(kaba::IExporter* e) {
+	kaba::make_context_public(e);
 
 	e->package_info("xedit", "0.1");
 
